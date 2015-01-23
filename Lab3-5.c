3ec31916384917ef6cececaaf69a8a04892544ee
Seksan Neramitthanasombat <sek.cgnz@gmail.com>
2015-01-23 23:45:21 +0700

    Lab3

--- Lab3-5.c
+++ Lab3-5.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
-
+#include <math.h>
+#define PI 3.1416
 
 int main()
 {
-	float c, r;
+	double cir, r;
 
-	scanf("%f", &c);
+	scanf("%lf", &cir);
 
-	r = c/2;
+	r = cir/2;
 
-	printf("Volume: %.4f m^3\n", (4.0/3.0*3.1416*(r*r*r))/1000000.0);
-	printf("Surface Area: %.4f m^2\n", 4.0*3.1416*(r*r)/10000.0);
+	printf("Volume: %.4lf m^3\n", (4*PI*pow(r, 3))/3.0/1000000.0);
+	printf("Surface Area: %.4lf m^2\n", 4*PI*pow(r, 2)/10000.0);
 
 	return 0;
 }
