e0bab325600b0cd4d886daa36a7fdea8088cdc81
Seksan Neramitthanasombat <sek.cgnz@gmail.com>
2015-02-13 23:01:46 +0700

    Lab6

--- Lab6-6.c
+++ Lab6-6.c
@@ -4,8 +4,6 @@
 
 int main()
 {
-	/* Input name and convert all character to lowercase for sorting */
-
 	char name[20][30];
 
 	int i, j;
@@ -18,18 +16,23 @@ int main()
 		char temp[len];
 		for (j = 0; j < len; j++)
 		{
-			temp[j] = tolower(input[j]);
+			if (j == 0 || (isalpha(input[j]) && input[j - 1] == ' '))
+			{
+				temp[j] = toupper(input[j]);
+			}
+			else
+			{
+				temp[j] = tolower(input[j]);
+			}
 		}
-		temp[len] = '\0';
 
+		temp[len] = '\0';
 		strcpy(name[i], temp);
 	}
 
-	/* Sort name by character from a-z */
-
-	char temp[30];
 	for (i = 0; i < 20; i++)
 	{
+		char temp[30];
 		for (j = i + 1; j < 20; j++)
 		{
 			if (strcmp(name[i], name[j]) > 0)
@@ -41,32 +44,9 @@ int main()
 		}
 	}
 
-	/* Output with adjusted all characters in each name */
-
-	int k;
 	for (i = 0; i < 20; i++)
 	{
-		char n[2][30];
-		sscanf(name[i], "%s %s", n[0], n[1]);
-		for (j = 0; j < 2; j++)
-		{
-			int len = strlen(n[j]);
-			for (k = 0; k < len; k++)
-			{
-				if (k == 0)
-				{
-					putchar(toupper(n[j][k]));
-				}
-				else
-				{
-					putchar(tolower(n[j][k]));
-				}
-			}
-
-			putchar(' ');
-		}
-
-		putchar('\n');
+		puts(name[i]);
 	}
 
 	return 0;
