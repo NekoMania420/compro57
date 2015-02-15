0f8c539c12f2373016e0210826c429ef714613e0
Seksan Neramitthanasombat <sek.cgnz@gmail.com>
2015-02-16 05:48:29 +0700

    Challenge

--- Prime Number_1.c
+++ Prime Number_1.c
@@ -1,27 +1,33 @@
 #include <stdio.h>
 #include <math.h>
 
-void isPrime(int );
+int isPrime(int );
 
 int main()
 {
 	int num;
 	scanf("%d", &num);
 
-	isPrime(num);
+	isPrime(num) ? puts("true") : puts("false");
 
 	return 0;
 }
 
-void isPrime(int n)
+int isPrime(int n)
 {
-	int dividable = 0;
+	if (n < 2)
+	{
+		return 0;
+	}
 
 	int i;
 	for (i = 2; i <= n / 2; i++)
 	{
-		n % i == 0 ? dividable = 1 : 0;
+		if (n % i == 0)
+		{
+			return 0;
+		}
 	}
 
-	dividable == 0 ? puts("true") : puts("false");
+	return 1;
 }
